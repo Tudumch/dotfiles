24e826a74d6700a4e9221b3df0238b746110f60f
MSI-Arch <567-45@mail.ru>
2022-09-05 21:23:33 +0300

    Fix for screen autolock

--- i3/i3lockscript.sh
+++ i3/i3lockscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # скрипт вызывается из файла ~/.xsession для блокировки экрана
 
-overlay=~/linux_configs/arch/configs/i3/overlay.png
+overlay=~/.config/i3/overlay.png
 rm /tmp/currentworkspace.png /tmp/currentworkspaceblur.png
 scrot /tmp/currentworkspace.png
 convert /tmp/currentworkspace.png -blur 0x5 /tmp/currentworkspaceblur.png
