a10afb6583ce528658fc6636312cf1448eede57f
serg <567-45@mail.ru>
2023-12-12 00:49:09 +0300

    feat(Installation): change Python installation path in auto-installation script

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ then
 fi
 
 echo
-echo If you agree - press any key, if no - press CTRL+C
+echo If you agree - press ENTER, if no - press CTRL+C
 read
 
 sudo pacman -Sy
@@ -39,8 +39,8 @@ sudo pacman -S xclip --noconfirm # for work with system clipboard
 sudo pacman -S ctags --noconfirm # for nvim tagbar-plugin
 sudo pacman -S ripgrep --noconfirm # for search in Telescope-plugin
 sudo pacman -S pyright --noconfirm # for python autocomplition
-python -m venv path/to/virtualenvs/debugpy # python debug
-path/to/virtualenvs/debugpy/bin/python -m pip3 install debugpy # python debug
+python -m venv ~/.python/path/to/virtualenvs/debugpy # python debug
+~/.python/path/to/virtualenvs/debugpy/bin/python -m pip3 install debugpy # python debug
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim  # nvim plugin-manager Packer
 
 # VIFM (filemanager)
